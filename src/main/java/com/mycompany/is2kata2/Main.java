85b2bc08e0728cce517ad12849a3e1b2f39a90bb
ASQ01 <alvarosq2001@gmail.com>
2021-09-30 11:00:20 +0100

    Kata2v2

--- src/main/java/com/mycompany/is2kata2/Main.java
+++ src/main/java/com/mycompany/is2kata2/Main.java
@@ -7,15 +7,8 @@ public class Main {
         int[] data = new int[]{23, 45, 8, 24, 30, 6, 35, 7, 0, 10, 11, 2, 77, 34, 32};
         HashMap<Integer, Integer> histogram = new HashMap<Integer, Integer>();
         
-        for (int i = 0; i < data.length; i++) {
-            if(histogram.containsKey(data[i])){
-                histogram.put(data[i], histogram.get(data[i])+1);
-            }
-            
-            else{
-                histogram.put(data[i], 1);
-            }
-            
+        for (int number : data) {
+            histogram.put(number, histogram.containsKey(number) ? histogram.get(number) + 1 : 1);
         }
         
         for (Integer key : histogram.keySet()){
